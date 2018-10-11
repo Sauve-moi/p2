e629a6a893b700164101bff45868833f0df373a3
zgao9 <zgao9@ncsu.edu>
2018-10-11 11:43:12 -0400

    addddd

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,5 @@
 class Interest < ApplicationRecord
-  after_initialize :inserttoptb
+  after_initialize :ptb
   def ptb
     PotentialBuyers.new(house_id: self.house_id, buyer_id: self.buyer_id)
   end
